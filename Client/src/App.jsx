fa347f48a0a8cef4906728d0d3dc7490a1368ee3
gyanavardhana <gyanavardhanmamidisetti@gmail.com>
2024-03-13 16:52:19 +0530

    corrected structure

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import HomePage from './HomePage'
-import Login from './Login'
-import Signup from './Signup';
+import HomePage from './Components/Homepage/HomePage';
+import Login from './Components/Login/Login';
+import Signup from './Components/Signup/Signup';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
