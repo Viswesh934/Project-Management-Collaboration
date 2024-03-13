fc326a4834a15493beeb04481f03e481119cfe26
gyanavardhana <gyanavardhanmamidisetti@gmail.com>
2024-03-13 23:33:50 +0530

    contact us functionality added

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import HomePage from './Components/Homepage/HomePage';
 import Login from './Components/Login/Login';
 import Signup from './Components/Signup/Signup';
+import ContactUs from './Components/ContactUs/ContactUs';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/contactus" element={<ContactUs />} />
         </Routes>
       </div>
   )
--- Server/routes/memberroutes.js
+++ Server/routes/memberroutes.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const memberController = require('../Controllers/MemberController');
+const ContactUsController = require('../Controllers/ContactUsController');
+const cookeParser = require('cookie-parser');
 
+router.use(cookeParser());
 router.use(express.json());
 router.post('/signup', memberController.memberSignup);
 router.post('/login', memberController.memberLogin);
 router.post('/logout', memberController.memberLogout);
+router.post('/contactus', ContactUsController.contactUs);
 
 module.exports = router;
