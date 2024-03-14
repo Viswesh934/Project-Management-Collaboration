7f236fea9e9ead2f51c35daedbecab0949c10e04
M. Gyanavardhan <89439095+gyanavardhana@users.noreply.github.com>
2024-03-14 10:13:45 +0530

    Update App.jsx

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,6 +3,7 @@ import HomePage from './Components/Homepage/HomePage';
 import Login from './Components/Login/Login';
 import Signup from './Components/Signup/Signup';
 import ContactUs from './Components/ContactUs/ContactUs';
+import ResourcesPage from './Components/ResourcePage/ResourcePage';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/contactus" element={<ContactUs />} />
+          <Route path="/resources" element={<ResourcesPage/>}/>
         </Routes>
       </div>
   )
